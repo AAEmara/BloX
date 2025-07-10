6b1536b7f040e4435a0b6ef5777bef9b50b67bf6
abdelsalam101 <abdelsalamhassan15@gmail.com>
2025-07-10 21:52:13 +0300

    Implemented Admin Post CRUD with Jazzmin list view

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,20 @@
 from django.contrib import admin
 from .models import Post, Category, Tag
 
-# Register your models here.
-admin.site.register(Post)
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'publish_date', 'category')
+    search_fields = ('title', 'content')
+    list_filter = ('category', 'publish_date')
+    ordering = ('-publish_date',)
+    
+    def image_tag(self, obj):
+        if obj.image:
+            return format_html('<img src="{}" width="50" height="50" />', obj.image.url)
+        return "-"
+    image_tag.short_description = 'Image'
+    
+    
 admin.site.register(Category)
 admin.site.register(Tag)
