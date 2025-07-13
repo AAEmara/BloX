2d5e482517c96950d36a477bf19970a48d1c1780
Kerolos Nabil <kerolosnabil247@gmail.com>
2025-07-13 13:59:03 +0300

    fix(auth): fix blog_auth
    fixed the following issues in blog_auth
       - Fix the password in the user form.
       - Fix the validationError in the user form.
       - Fix the the grouping in the condition inside the user form.
    
    Fixes #2

--- blog_auth/forms.py
+++ blog_auth/forms.py
@@ -6,7 +6,7 @@ class UserForm(UserCreationForm):
 
     class Meta:
         model = CustomUser
-        fields = ('username', 'email', 'first_name', 'last_name', 'password1', 'password2')
+        fields = ('username', 'email', 'first_name', 'last_name')
 
     def clean_email(self):
         email = self.cleaned_data.get('email')
@@ -18,10 +18,10 @@ class UserForm(UserCreationForm):
 
         # this is to do the clean function in the UserCreationForm to get the cleaned (validated) data
         cleaned_data = super().clean()
-        password1 = cleaned_data.get('password1')
-        password2 = cleaned_data.get('password2')
-        if password1 and password2 and password1 != password2:
-            raise forms.ValueError("Passwords don't match.")
+        password = cleaned_data.get('password')
+        password_confirmation = cleaned_data.get('password2')
+        if (password and password_confirmation) and (password != password_confirmation):
+            raise forms.ValidationError("Passwords don't match.")
         
         return cleaned_data
         
