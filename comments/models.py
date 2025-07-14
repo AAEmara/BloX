198efdcc36375fdff0303660aa23d15ab8ee2d90
Mohamed Hassan <mohamed952741@gmail.com>
2025-07-14 22:17:36 +0300

    feat(comments): Add Comment model - added comment model Closes #25

---
+++ comments/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.contrib.auth.models import User
+from blog.models import Post
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    parent = models.ForeignKey(
+        'self',
+        null=True,
+        blank=True,
+        on_delete=models.CASCADE,
+        related_name='replies'
+    )
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def is_reply(self):
+        return self.parent is not None
+
+    def __str__(self):
+        return f"{self.user.username} - {self.content}"
