d333a6f344b8e4dd9a5641d3cfb331ea4ac4ba61
AAEmara <abdelrahmanemara18@gmail.com>
2025-07-15 09:29:46 +0300

    feat(header-state): added header state logic
    Added the following logic to the header states:
      - If the user is authenticated but not a staff, then they get a
        "Logout" button.
      - If the user is authenticated but not a staff, then they get a
        "Logout" and a "Manage Blog" button.
      - If the user is a guest, then they get a "Login" and "Register"
        buttons.
    
    Migration error was fixed by:
      - Removing the second migration inside the `blog` app, where it was
        a duplicate migration for the "likes" field.
      - Made the first migration of the `comments` app to be dependent on
        the first migration of the `blog` app.
    
    Updated the templates to be more dynamic and follow the three layer
    templating method.
    
    Created a fixture for the newly created user examples:
      - Ordinary user data.
      - Admin user data.
    
    Fixed `comments` app's model.py file, where it currently
    uses the CustomUser model.
    
    Closes #40

--- comments/models.py
+++ comments/models.py
@@ -1,9 +1,9 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.conf import settings
 from blog.models import Post
 
 class Comment(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
     parent = models.ForeignKey(
         'self',
