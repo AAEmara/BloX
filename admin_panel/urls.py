6feb2d6f8f430b1ec43c14144bb916d03f8bed94
Ahmed Ramadan <ahmedramadan8050@gmail.com>
2025-07-20 05:37:40 +0300

    feat: block and unblock users Closes #35

--- admin_panel/urls.py
+++ admin_panel/urls.py
@@ -5,5 +5,6 @@ app_name = 'admin_panel'
 
 urlpatterns = [
     path('admins/', views.user_list, name='user_list'),
-  
+    path('admins/block/<int:user_id>/', views.block_user, name='block_user'),
+    path('admins/unblock/<int:user_id>/', views.unblock_user, name='unblock_user'),
 ]
