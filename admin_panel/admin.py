56b16069f6d4bddc78abdaf299b84d428f207c35
Ahmed Ramadan <ahmedramadan8050@gmail.com>
2025-07-20 05:27:16 +0300

    fix: complete missing logic for issue #33

--- admin_panel/admin.py
+++ admin_panel/admin.py
@@ -1,7 +1,6 @@
 from django.contrib.admin.views.decorators import staff_member_required
 from django.shortcuts import render
 from blog_auth.models import CustomUser
-from django.contrib import admin
 
 
 @staff_member_required
@@ -9,4 +8,3 @@ def user_list(request):
     users = CustomUser.objects.all()
     return render(request, 'admin_panel/user_list.html', {'users': users})
 
-admin.site.register(CustomUser)
